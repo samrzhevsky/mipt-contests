dff9f633784aa57a01878cc7d161d435b752857d
Sam Rzhevsky <samrzhevsky@gmail.com>
2022-12-19 17:59:53 +0300

    fixed diff

--- Приплюснутые программисты/diff.cpp
+++ Приплюснутые программисты/diff.cpp
@@ -1,5 +1,5 @@
 template <typename T>
-int *diff_of_arrays(T arr1[], T arr2[], T *res, size_t size, T start = 0)
+T *diff_of_arrays(T arr1[], T arr2[], T *res, size_t size, T start = 0)
 {
     for (size_t i = 0; i < size; i++)
         res[i] = arr1[i] - arr2[i] + start;
